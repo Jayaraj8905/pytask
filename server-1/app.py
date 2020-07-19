50e6bbf3f0e3beb42b914af6f54fe1747154e91f
Jayaraj <jayaraj8905@gmail.com>
2020-07-19 19:34:13 +0800

    CSV Conversion logic

--- server-1/app.py
+++ server-1/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import send_file
 from flask import make_response
+import requests
 import io, csv
+import json
 
 app = Flask(__name__)
 
@@ -11,13 +13,19 @@ def default():
 
 @app.route("/download")
 def download():
+    # Initialize the csv
     dest = io.StringIO()
     writer = csv.writer(dest)
 
-    writer.writerow(['Name', 'Profession'])
-    writer.writerow(['Derek', 'Software Developer'])
-    writer.writerow(['Steve', 'Software Developer'])
-    writer.writerow(['Paul', 'Manager'])
+    # Request the data from server
+    r = requests.get("http://localhost:5002/parse")
+    parsedData = json.loads(r.text)
+
+    # Iterate and construct the data
+    for data in parsedData:  
+        writer.writerow([str(data['id']), str(data['president']), data['nm'], data['pp'], data['tm']])
+    
+    # Respond the data
     output = make_response(dest.getvalue())
     output.headers["Content-Disposition"] = "attachment; filename=export.csv"
     output.headers["Content-type"] = "text/csv"
